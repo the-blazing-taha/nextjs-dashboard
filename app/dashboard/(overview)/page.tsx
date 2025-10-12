97daf5330b77f6d027fcdedab2d074405cea2d28
the-blazing-taha <tahmad.bscs21seecs@seecs.edu.pk>
2025-10-12 16:49:04 +0500

    Searching and pagination done

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,12 +8,12 @@ import { CardsSkeleton,LatestInvoicesSkeleton, RevenueChartSkeleton} from '@/app
 import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
-    const {
-        totalPaidInvoices,
-        totalPendingInvoices,
-        numberOfInvoices,
-        numberOfCustomers,
-    } = await fetchCardData();
+    // const {
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    //     numberOfInvoices,
+    //     numberOfCustomers,
+    // } = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,14 +23,14 @@ export default async function Page() {
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
-                 <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
-                 <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-                 <Card
+                 {/*<Card title="Collected" value={totalPaidInvoices} type="collected" />*/}
+                 {/*<Card title="Pending" value={totalPendingInvoices} type="pending" />*/}
+                 {/*<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />*/}
+                 {/*<Card*/}
           title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        {/*  value={numberOfCustomers}*/}
+        {/*  type="customers"*/}
+        {/*/>*/}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                  {/*<RevenueChart revenue={revenue}  />*/}
