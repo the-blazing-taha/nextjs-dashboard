f4cb9c8d0cfc24433d7b2d022bcf2f3487ccc3bc
the-blazing-taha <tahmad.bscs21seecs@seecs.edu.pk>
2025-10-15 12:24:06 +0500

    Metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import React from "react";
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Course Dashboard, built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
